65655978f6c7424f53916892452a169dfb401964
Anna Solemani <anna@solemani.com>
2019-10-29 16:16:11 +0200

    Dynamic CV
    I wrote the html and python codes but can't work out how to combine them

---
+++ dycv.py
@@ -0,0 +1,11 @@
+my_name= "Anna Solemani"
+job_title= "Senior Editor"
+job_length= 6
+task_1= "Editing"
+task_2= "Copyediting"
+task_3= "Proofreading"
+
+print("{} CV".format(my_name))
+print(job_title)
+print("I have {} years' experience as a {}. I offer: {}, {}, {}".format(job_length,job_title, task_1,task_2,task_3))
+
